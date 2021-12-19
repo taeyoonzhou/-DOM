53542cdf1c0dc849d535447ca48049f540271a09
taeyoon <13701488156@gmail.com>
2021-12-19 20:56:58 +0800

    ok

---
+++ src/dom.js
@@ -0,0 +1,27 @@
+window.dom = {
+    create(tagName) {
+        return document.createElement(tagName);
+    },
+    find(selector){
+        return document.querySelectorAll(selector)
+    },
+    style(node,name,value){
+        if(arguments.length===3){
+            node.style[name]=value
+        }else if(arguments.length===2){
+            if(typeof name ==='string'){
+                return node.style[name]
+            }else if(name instanceof Object){
+                const Object=name
+                for(let key in Object){
+                    node.style[key]=Object[key]
+                }
+            }
+        }
+    } ,
+    each(nodeList,fn) {
+        for(let i=0;i<nodeList.length;i++) {
+            fn.call(null,nodeList[i])
+        }
+    }
+};
